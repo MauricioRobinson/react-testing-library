05320d8f7bb1307c7c4064408c1b28893a2e0fc5
Mauricio Robinson <marsjr97@gmail.com>
2023-03-09 23:07:48 +0000

    Testing async components

---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import FollowersList from "../FollowersList";
+import { BrowserRouter } from "react-router-dom";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("Testing the FollowersList component", () => {
+  it("Should render the first card", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
+
+  it("Should render multiple followers", async () => {
+    render(<MockFollowersList />);
+    const followerDivElements = await screen.findAllByTestId(/^follower-item/i);
+    expect(followerDivElements.length).toBe(5);
+  });
+});
