f409143202cda667fcc1bc3c679a8eb4986d1a13
Mauricio Robinson <marsjr97@gmail.com>
2023-03-09 13:35:07 +0000

    Describing the tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-it("Should render same text passed into title prop", async () => {
-  render(<Header title="My Header" />);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
+describe("Testing the Header component", () => {
+  it("Should render same text passed into title prop", async () => {
+    render(<Header title="My Header" />);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
 
 // it("Should render a heading element", async () => {
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -10,38 +10,40 @@ export const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
   );
 };
 
-it("Should render the correct amount of tasks and the word task must finish with s", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe("Testing the TodoFooter component", () => {
+  it("Should render the correct amount of tasks and the word task must finish with s", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-it("Should render 'task' when the number of incmplete tasks is 1", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+  it("Should render 'task' when the number of incmplete tasks is 1", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-it("Should return a true value", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeTruthy();
-});
+  it("Should return a true value", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeTruthy();
+  });
 
-it("Should be visible to the user", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeVisible();
-});
+  it("Should be visible to the user", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeVisible();
+  });
 
-it("Should render a (p) tag", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML("p");
-});
+  it("Should render a (p) tag", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toContainHTML("p");
+  });
 
-it("Should not render 'task' when the number of incmplete tasks is 1", async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={2} />);
-  const paragraphElement = screen.getByText(/2 tasks left/i);
-  expect(paragraphElement.textContent).not.toBe("2 task left");
+  it("Should not render 'task' when the number of incmplete tasks is 1", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={2} />);
+    const paragraphElement = screen.getByText(/2 tasks left/i);
+    expect(paragraphElement.textContent).not.toBe("2 task left");
+  });
 });
