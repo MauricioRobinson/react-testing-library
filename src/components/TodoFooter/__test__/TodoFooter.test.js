bbb272c5a650245ba6398f1875083da3fbd5389f
Mauricio Robinson <marsjr97@gmail.com>
2023-03-09 13:16:36 +0000

    Adding assertions to my tests

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
+
+export const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+it("Should render the correct amount of tasks and the word task must finish with s", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it("Should render 'task' when the number of incmplete tasks is 1", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it("Should return a true value", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeTruthy();
+});
+
+it("Should be visible to the user", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeVisible();
+});
+
+it("Should render a (p) tag", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toContainHTML("p");
+});
+
+it("Should not render 'task' when the number of incmplete tasks is 1", async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={2} />);
+  const paragraphElement = screen.getByText(/2 tasks left/i);
+  expect(paragraphElement.textContent).not.toBe("2 task left");
+});
